dba812a87c27bf0c424a0807ce8c595020556c82
Xain Storey <xhs893@gmail.com>
2019-04-08 16:34:55 +0100

    Created method to top up balance of card, passed test using 'change_by' RSpec method'
    Co-authored-by: Gui Heurich <guiheurich@gmail.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it "increments the balance" do
+    amount = 10
+     expect { subject.top_up(amount) }.to change { subject.balance }.from(0).to(10)
+  end
+
 end
